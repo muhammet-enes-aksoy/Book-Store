e3beb302e978ec93bd36797d945684aca2f30e0b
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-09 20:22:46 +0300

    Initial Commit

---
+++ BookStore/Program.cs
@@ -0,0 +1,42 @@
+using BookStore.Api.Common;
+using BookStore.Api.DbOperations;
+using BookStore.DbOperations;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDB"));
+
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+}
+
+app.Run();
