dcef2659d0ab5137f62e8a18dea2577eca552aa3
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-09 20:25:31 +0300

    Create MappingProfile.cs

---
+++ BookStore/Common/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BookStore.Api.BookOperations.GetBookDetail;
+using BookStore.Api.BookOperations.GetBooks;
+using BookStore.BookOperations.CreateBook;
+using BookStore.Common;
+
+namespace BookStore.Api.Common;
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CreateBookModel, Book>();
+
+        CreateMap<Book, BooksViewModel>()
+        .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()))
+        .ForMember(dest => dest.PublishDate, opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")));
+
+        CreateMap<Book, BookDetailViewModel>()
+            .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()))
+            .ForMember(dest => dest.PublishDate, opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")));
+    }
+}
