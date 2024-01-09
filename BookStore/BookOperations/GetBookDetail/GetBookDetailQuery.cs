8655c2216ddf87d7d7807d757a3d3d868db147a0
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-09 20:25:24 +0300

    Create queries

---
+++ BookStore/BookOperations/GetBookDetail/GetBookDetailQuery.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BookStore.Api.BookOperations.GetBookDetail;
+using BookStore.Api.DbOperations;
+
+namespace BookStore.BookOperations.GetBookDetail;
+public class GetBookDetailQuery(BookStoreDbContext dbContext, IMapper mapper)
+{
+    private readonly BookStoreDbContext dbContext = dbContext;
+    private readonly IMapper mapper = mapper;
+
+    public int BookId { get; set; }
+
+    public async Task<BookDetailViewModel> HandleAsync()
+    {
+        var book = await dbContext.Books.FindAsync(BookId)
+            ?? throw new InvalidOperationException($"Book Not Found with id: {BookId}");
+
+        return mapper.Map<BookDetailViewModel>(book);
+    }
+}
---
+++ BookStore/BookOperations/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BookStore.Api.DbOperations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api.BookOperations.GetBooks;
+public class GetBooksQuery(BookStoreDbContext dbContext, IMapper mapper)
+{
+    private readonly BookStoreDbContext dbContext = dbContext;
+    private readonly IMapper mapper = mapper;
+
+    public async Task<List<BooksViewModel>> HandleAsync()
+    {
+        var bookList = await dbContext.Books.OrderBy(x => x.Id).ToListAsync();
+
+        return mapper.Map<List<BooksViewModel>>(bookList);
+    }
+}
