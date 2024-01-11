3060e369444575679dd9b65f451c33f49966e2eb
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-11 23:41:19 +0300

    Update DataGenerator.cs

--- BookStore/DbOperations/DataGenerator.cs
+++ BookStore/DbOperations/DataGenerator.cs
@@ -12,6 +12,43 @@ public static class DataGenerator
         if (context.Books.Any())
             return;
 
+        context.Authors.AddRange(
+            new Author
+            {
+                //Id = 1,
+                Name = "Fyodor",
+                Surname = "Dostoevsky",
+                Birthday = new DateTime(1821, 11, 11)
+            },
+            new Author
+            {
+                //Id = 2,
+                Name = "Terry",
+                Surname = "Pratchett",
+                Birthday = new DateTime(1948, 4, 28)
+            },
+            new Author
+            {
+                //Id = 3,
+                Name = "T.S.",
+                Surname = "Eliot",
+                Birthday = new DateTime(1888, 9, 26)
+            },
+            new Author
+            {
+                //Id = 4,
+                Name = "Gabriel",
+                Surname = "García Márquez",
+                Birthday = new DateTime(1927, 3, 6)
+            },
+            new Author
+            {
+                //Id = 5,
+                Name = "Art",
+                Surname = "Spiegelman",
+                Birthday = new DateTime(1948, 2, 15)
+            }
+        );
         context.Genres.AddRange(
                 new Genre
                 {
@@ -44,6 +81,7 @@ public static class DataGenerator
                 {
                     Title = "Crime and Punishment",
                     GenreId = 5, // Novel
+                    AuthorId = 1,
                     PageCount = 400,
                     PublishDate = new DateTime(1866, 1, 1)
                 },
@@ -51,6 +89,7 @@ public static class DataGenerator
                 {
                     Title = "Good Omens",
                     GenreId = 6, // Humor
+                    AuthorId = 2,
                     PageCount = 333,
                     PublishDate = new DateTime(1990, 5, 1)
                 },
@@ -58,6 +97,7 @@ public static class DataGenerator
                 {
                     Title = "The Waste Land",
                     GenreId = 4, // Poetry
+                    AuthorId = 3,
                     PageCount = 200,
                     PublishDate = new DateTime(1922, 10, 22)
                 },
@@ -65,6 +105,7 @@ public static class DataGenerator
                 {
                     Title = "One Hundred Years of Solitude",
                     GenreId = 5, // Novel
+                    AuthorId = 4,
                     PageCount = 417,
                     PublishDate = new DateTime(1967, 5, 30)
                 },
@@ -72,6 +113,7 @@ public static class DataGenerator
                 {
                     Title = "Maus",
                     GenreId = 2, // Graphic Novel
+                    AuthorId = 5,
                     PageCount = 296,
                     PublishDate = new DateTime(1986, 1, 1)
                 }
